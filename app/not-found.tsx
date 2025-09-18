be27a72ed80db59bd97f32b40ab630df98e46ce3
huszkacs <huszkacs@gmail.com>
2025-09-18 17:36:57 +0200

    Fixes not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function NotFound() {
   return (
     <div>
-      <header className="">
+      <header className="text-black">
         <h1 className="text-center pt-10 text-3xl sm:text-3xl md:text-4xl 2xl:text-5xl font-medium">Page Not Found</h1>
         <Link href="/" className="block mx-auto max-w-sm sm:max-w-none text-center pt-10 text-md sm:text-lg md:text-xl 2xl:text-xl underline underline-offset-2">Go back to home page</Link>
       </header>
